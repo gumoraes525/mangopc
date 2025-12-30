276d0b8e5e496c843eb78897537f4caec3a3cc59
gumoraes525 <gumenezes2019@googlemail.com>
2025-12-30 20:30:19 -0300

    Rename m68k_cpu.c to m68k_cpu.cc and update includes

--- src/cpu/cpu_m68k/m68k_cpu.c
+++ src/cpu/cpu_m68k/m68k_cpu.c
@@ -1,28 +1,33 @@
 /*
  * Mango68k
- * m68k_cpu.c
+ * m68k_cpu.cc
  *
  * Based on PearPC structure, rewritten for Motorola 68000
  */
 
-#include <string.h>
-#include <stdio.h>
+#include <cstring>
+#include <cstdio>
 
 #include "cpu/cpu.h"
 #include "cpu/m68k_cpu.h"
 #include "cpu/m68k_decode.h"
+#include "cpu/m68k_exceptions.h"
 #include "debug/debugger.h"
+#include "mem/memory.h"   // you WILL need this
 
 M68K_CPU_State gCPU;
-Debugger *gDebugger = NULL;
+Debugger *gDebugger = nullptr;
 
 bool m68k_cpu_init(void)
 {
-    memset(&gCPU, 0, sizeof(gCPU));
+    std::memset(&gCPU, 0, sizeof(gCPU));
 
-    /* 68000 reset state */
-    gCPU.pc = 0x00000000;
-    gCPU.sr = 0x2700; /* supervisor, interrupts masked */
+    /* 68000 RESET sequence */
+    gCPU.a[7] = mem_read_long(0x00000000); /* SSP */
+    gCPU.pc   = mem_read_long(0x00000004); /* PC */
+    gCPU.sr   = 0x2700;                    /* Supervisor, IPL=7 */
+
+    gCPU.stopped = false;
 
     return true;
 }
@@ -31,38 +36,38 @@ void m68k_cpu_run(void)
 {
     gDebugger = new Debugger();
 
-    while (true) {
+    while (!gCPU.stopped) {
         uint16 opcode;
 
-        /* fetch */
+        /* Fetch */
         opcode = m68k_read_word(gCPU.pc);
 
-        /* decode + execute */
+        /* Decode + Execute */
         m68k_exec_opcode(opcode);
 
-        /* PC increment happens in exec (variable length) */
+        /* Exceptions handled centrally */
+        if (gCPU.exception_pending) {
+            m68k_handle_exception();
+        }
+
+        /* Debugger hook */
+        if (gDebugger && gDebugger->shouldBreak()) {
+            gDebugger->enter();
+        }
     }
 }
 
 void m68k_cpu_stop(void)
 {
-    /* placeholder */
+    gCPU.stopped = true;
 }
 
-uint32 m68k_cpu_get_pc(int cpu)
+uint32 m68k_cpu_get_pc(int /* cpu */)
 {
     return gCPU.pc;
 }
 
-void m68k_cpu_set_pc(int cpu, uint32 value)
+void m68k_cpu_set_pc(int /* cpu */, uint32 value)
 {
     gCPU.pc = value;
-}
-
-uint16 m68k_fetch_word(void)
-{
-    uint16 val;
-    mem_read_word(gCPU.pc, &val);
-    gCPU.pc += 2;
-    return val;
 }
