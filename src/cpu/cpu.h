5a61ad558a57233c5ffbc814e7c45efbe069b857
gumoraes525 <gumenezes2019@googlemail.com>
2025-12-30 20:37:04 -0300

    Enhance cpu.h with comments and new function
    Added comments for clarity and introduced cpu_lower_interrupt function.

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -10,6 +10,8 @@ bool cpu_init(void);
 void cpu_init_config(void);
 
 void cpu_stop(void);
+
+/* Optional / architecture dependent */
 void cpu_wakeup(void);
 
 /*
@@ -27,8 +29,8 @@ void   cpu_set_pc(uint32 newvalue);
 /*
  * Register access (debugger / introspection)
  */
-uint32 cpu_get_dreg(int index);   // D0–D7
-uint32 cpu_get_areg(int index);   // A0–A7
+uint32 cpu_get_dreg(int index);   /* D0–D7 */
+uint32 cpu_get_areg(int index);   /* A0–A7 */
 void   cpu_set_dreg(int index, uint32 value);
 void   cpu_set_areg(int index, uint32 value);
 
@@ -41,7 +43,9 @@ void   cpu_set_sr(uint16 value);
 /*
  * Interrupts / exceptions
  */
-void cpu_raise_interrupt(int level);
-void cpu_clear_interrupt(int level);
+void cpu_raise_interrupt(int level);   /* 1–7 */
+void cpu_lower_interrupt(int level);
 
-#endif
+void cpu_raise_exception(int vector);
+
+#endif /* __CPU_H__ */
