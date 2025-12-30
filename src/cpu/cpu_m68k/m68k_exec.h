0dc106ffdc4d3468b5a15b08293ec254583b4e3f
gumoraes525 <gumenezes2019@googlemail.com>
2025-12-30 20:34:36 -0300

    Add header file for M68K opcode execution

---
+++ src/cpu/cpu_m68k/m68k_exec.h
@@ -0,0 +1,9 @@
+#ifndef __M68K_EXEC_H__
+#define __M68K_EXEC_H__
+
+#include "system/types.h"
+
+/* Execute a decoded opcode */
+void m68k_exec_opcode(uint16 opcode);
+
+#endif /* __M68K_EXEC_H__ */
