6464bb88290e2b49249a4b5180812301cb8be86e
gumoraes525 <gumenezes2019@googlemail.com>
2025-12-30 20:09:02 -0300

    Refactor CPU header to update function declarations
    Removed deprecated PowerPC function declarations and added new CPU lifecycle and execution functions.

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -1,53 +1,47 @@
-/*
- *	PearPC
- *	cpu.h
- *
- *	Copyright (C) 2003, 2004 Sebastian Biallas (sb@biallas.net)
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License version 2 as
- *	published by the Free Software Foundation.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 #ifndef __CPU_H__
 #define __CPU_H__
 
 #include "system/types.h"
 
-uint64	ppc_get_clock_frequency(int cpu);
-uint64	ppc_get_bus_frequency(int cpu);
-uint64	ppc_get_timebase_frequency(int cpu);
+/*
+ * CPU lifecycle
+ */
+bool cpu_init(void);
+void cpu_init_config(void);
 
-bool	ppc_cpu_init();
-void	ppc_cpu_init_config();
-
-void	ppc_cpu_stop();
-void	ppc_cpu_wakeup();
-
-void	ppc_machine_check_exception();
-
-void	ppc_cpu_raise_ext_exception();
-void	ppc_cpu_cancel_ext_exception();
+void cpu_stop(void);
+void cpu_wakeup(void);
 
 /*
- * May only be called from within a CPU thread.
+ * CPU execution
+ * May only be called from within the CPU thread
  */
+void cpu_run(void);
 
-void	ppc_cpu_run();
-uint32	ppc_cpu_get_gpr(int cpu, int i);
-void	ppc_cpu_set_gpr(int cpu, int i, uint32 newvalue);
-void	ppc_cpu_set_msr(int cpu, uint32 newvalue);
-void	ppc_cpu_set_pc(int cpu, uint32 newvalue);
-uint32	ppc_cpu_get_pc(int cpu);
-uint32	ppc_cpu_get_pvr(int cpu);
+/*
+ * Program counter
+ */
+uint32 cpu_get_pc(void);
+void   cpu_set_pc(uint32 newvalue);
+
+/*
+ * Register access (debugger / introspection)
+ */
+uint32 cpu_get_dreg(int index);   // D0–D7
+uint32 cpu_get_areg(int index);   // A0–A7
+void   cpu_set_dreg(int index, uint32 value);
+void   cpu_set_areg(int index, uint32 value);
+
+/*
+ * Status register
+ */
+uint16 cpu_get_sr(void);
+void   cpu_set_sr(uint16 value);
+
+/*
+ * Interrupts / exceptions
+ */
+void cpu_raise_interrupt(int level);
+void cpu_clear_interrupt(int level);
 
 #endif
