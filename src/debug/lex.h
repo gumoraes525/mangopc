495c7228ea84528beeb68bf9ea4aee3c99672f5a
gumoraes525 <gumenezes2019@googlemail.com>
2025-12-30 20:47:49 -0300

    Refactor lex.h for C++ compatibility and reentrancy

--- src/debug/lex.h
+++ src/debug/lex.h
@@ -1,12 +1,24 @@
 #ifndef __LEX_H__
 #define __LEX_H__
 
-void *lex_current_buffer();
-int lex_current_buffer_pos();
-void lex_switch_buffer(void *buffer);
-void lex_delete_buffer(void *buffer);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "debug/debugparse.h"   /* for YYSTYPE */
+
+/* Flex buffer helpers */
+void *lex_current_buffer(void);
+int   lex_current_buffer_pos(void);
+void  lex_switch_buffer(void *buffer);
+void  lex_delete_buffer(void *buffer);
 void *lex_scan_string_buffer(const char *str);
 
-int yylex();
+/* Reentrant lexer */
+int yylex(YYSTYPE *lvalp);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __LEX_H__ */
